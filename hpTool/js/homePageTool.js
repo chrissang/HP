9db1d07db796e1a9e285439bb987a40a4fbef09c
Chris Sang <Chris@Chriss-MacBook-Pro.local>
2016-10-06 23:52:35 -0400

    updated sortable logic

--- hpTool/js/homePageTool.js
+++ hpTool/js/homePageTool.js
@@ -21,6 +21,101 @@ class Dependents {
                 });
             })
         };
+
+        ko.bindingHandlers.sortable = {
+            init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+                var el = document.getElementById('sortableContainer');
+
+                var sortable = new Sortable(el, {
+                    // group: "name",  // or { name: "...", pull: [true, false, clone], put: [true, false, array] }
+                    sort: true,  // sorting inside list
+                    delay: 0, // time in milliseconds to define when the sorting should start
+                    // disabled: false, // Disables the sortable if set to true.
+                    // store: {
+                    //
+                    // },
+                    animation: 150,  // ms, animation speed moving items when sorting, `0` — without animation
+                    handle: ".draggable",  // Drag handle selector within list items
+                    // filter: ".ignore-elements",  // Selectors that do not lead to dragging (String or Function)
+                    //draggable: ".item",  // Specifies which items inside the element should be sortable
+                    ghostClass: "sortable-ghost",  // Class name for the drop placeholder
+                    // chosenClass: "sortable-chosen",  // Class name for the chosen item
+                    // dataIdAttr: 'data-id',
+                    //
+                    // forceFallback: false,  // ignore the HTML5 DnD behaviour and force the fallback to kick in
+                    // fallbackClass: "sortable-fallback"  // Class name for the cloned DOM Element when using forceFallback
+                    // fallbackOnBody: false  // Appends the cloned DOM Element into the Document's Body
+                    //
+                    // scroll: true, // or HTMLElement
+                    // scrollSensitivity: 30, // px, how near the mouse must be to an edge to start scrolling.
+                    // scrollSpeed: 10, // px
+
+                    // setData: function (dataTransfer, dragEl) {
+                    //     dataTransfer.setData('Text', dragEl.textContent);
+                    // },
+                    //
+                    // // dragging started
+                    onStart: function (evt) {
+                        evt.oldIndex;  // element index within parent
+                        // console.log(viewModel.itemNumber() )
+                        
+                        console.log(evt.item )
+
+                        // var elements = document.querySelectorAll('.content');
+                        //
+                        // elements.forEach((el,index) => {
+                        //     //el.className += " content-shrink";
+
+                        // })
+                    },
+                    //
+                    // // dragging ended
+                    // onEnd: function (/**Event*/evt) {
+                    //     evt.oldIndex;  // element's old index within parent
+                    //     evt.newIndex;  // element's new index within parent
+                    // },
+                    //
+                    // // Element is dropped into the list from another list
+                    // onAdd: function (/**Event*/evt) {
+                    //     var itemEl = evt.item;  // dragged HTMLElement
+                    //     evt.from;  // previous list
+                    //     // + indexes from onEnd
+                    // },
+                    //
+                    // // Changed sorting within list
+                    // onUpdate: function (/**Event*/evt) {
+                    //     var itemEl = evt.item;  // dragged HTMLElement
+                    //     // + indexes from onEnd
+                    // },
+                    //
+                    // // Called by any change to the list (add / update / remove)
+                    // onSort: function (/**Event*/evt) {
+                    //     // same properties as onUpdate
+                    // },
+                    //
+                    // // Element is removed from the list into another list
+                    // onRemove: function (/**Event*/evt) {
+                    //     // same properties as onUpdate
+                    // },
+                    //
+                    // // Attempt to drag a filtered element
+                    // onFilter: function (/**Event*/evt) {
+                    //     var itemEl = evt.item;  // HTMLElement receiving the `mousedown|tapstart` event.
+                    // },
+                    //
+                    // // Event when you move an item in the list or between lists
+                    // onMove: function (/**Event*/evt) {
+                    //     // Example: http://jsbin.com/tuyafe/1/edit?js,output
+                    //     evt.dragged; // dragged HTMLElement
+                    //     evt.draggedRect; // TextRectangle {left, top, right и bottom}
+                    //     evt.related; // HTMLElement on which have guided
+                    //     evt.relatedRect; // TextRectangle
+                    //     // return false; — for cancel
+                    // }
+
+                });
+            }
+        };
     }
 }
 
@@ -40,7 +135,7 @@ ko.components.register('large-feature-module', {
                     <dd class="accordion-navigation">
                         <a data-bind="text: 'Large Feature Module '+accordionIndex, attr: { href: '#accordion'+accordionIndex, id: 'accordion-heading'+accordionIndex, role: 'tab' }" class="draggable"></a>
 
-                        <div data-bind="attr: { id: 'accordion'+accordionIndex, 'aria-labelledby': 'accordion-heading'+accordionIndex, role: 'tabpanel' }" class="content">
+                        <div data-bind="sortable: $parent, attr: { id: 'accordion'+accordionIndex, 'aria-labelledby': 'accordion-heading'+accordionIndex, role: 'tabpanel' }" class="content">
                             <div class="row">
                                 <div class="small-12 medium-4 columns">
                                     <label>Item #</label>
@@ -175,91 +270,7 @@ ko.components.register('homePageTool', {
             this.displayModules.push(this.selectedModule());
         }
 
-        ko.bindingHandlers.sortable = {
-            init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
-                var el = document.getElementById('sortableContainer');
 
-                var sortable = new Sortable(el, {
-                    // group: "name",  // or { name: "...", pull: [true, false, clone], put: [true, false, array] }
-                    sort: true,  // sorting inside list
-                    delay: 0, // time in milliseconds to define when the sorting should start
-                    // disabled: false, // Disables the sortable if set to true.
-                    // store: {
-                    //
-                    // },
-                    animation: 150,  // ms, animation speed moving items when sorting, `0` — without animation
-                    handle: ".draggable",  // Drag handle selector within list items
-                    // filter: ".ignore-elements",  // Selectors that do not lead to dragging (String or Function)
-                    //draggable: ".item",  // Specifies which items inside the element should be sortable
-                    ghostClass: "sortable-ghost",  // Class name for the drop placeholder
-                    // chosenClass: "sortable-chosen",  // Class name for the chosen item
-                    // dataIdAttr: 'data-id',
-                    //
-                    // forceFallback: false,  // ignore the HTML5 DnD behaviour and force the fallback to kick in
-                    // fallbackClass: "sortable-fallback"  // Class name for the cloned DOM Element when using forceFallback
-                    // fallbackOnBody: false  // Appends the cloned DOM Element into the Document's Body
-                    //
-                    // scroll: true, // or HTMLElement
-                    // scrollSensitivity: 30, // px, how near the mouse must be to an edge to start scrolling.
-                    // scrollSpeed: 10, // px
-
-                    // setData: function (dataTransfer, dragEl) {
-                    //     dataTransfer.setData('Text', dragEl.textContent);
-                    // },
-                    //
-                    // // dragging started
-                    onStart: function (evt) {
-                        evt.oldIndex;  // element index within parent
-                        console.log(element);
-                    },
-                    //
-                    // // dragging ended
-                    // onEnd: function (/**Event*/evt) {
-                    //     evt.oldIndex;  // element's old index within parent
-                    //     evt.newIndex;  // element's new index within parent
-                    // },
-                    //
-                    // // Element is dropped into the list from another list
-                    // onAdd: function (/**Event*/evt) {
-                    //     var itemEl = evt.item;  // dragged HTMLElement
-                    //     evt.from;  // previous list
-                    //     // + indexes from onEnd
-                    // },
-                    //
-                    // // Changed sorting within list
-                    // onUpdate: function (/**Event*/evt) {
-                    //     var itemEl = evt.item;  // dragged HTMLElement
-                    //     // + indexes from onEnd
-                    // },
-                    //
-                    // // Called by any change to the list (add / update / remove)
-                    // onSort: function (/**Event*/evt) {
-                    //     // same properties as onUpdate
-                    // },
-                    //
-                    // // Element is removed from the list into another list
-                    // onRemove: function (/**Event*/evt) {
-                    //     // same properties as onUpdate
-                    // },
-                    //
-                    // // Attempt to drag a filtered element
-                    // onFilter: function (/**Event*/evt) {
-                    //     var itemEl = evt.item;  // HTMLElement receiving the `mousedown|tapstart` event.
-                    // },
-                    //
-                    // // Event when you move an item in the list or between lists
-                    // onMove: function (/**Event*/evt) {
-                    //     // Example: http://jsbin.com/tuyafe/1/edit?js,output
-                    //     evt.dragged; // dragged HTMLElement
-                    //     evt.draggedRect; // TextRectangle {left, top, right и bottom}
-                    //     evt.related; // HTMLElement on which have guided
-                    //     evt.relatedRect; // TextRectangle
-                    //     // return false; — for cancel
-                    // }
-
-                });
-            }
-        };
 
       }
     },
@@ -279,7 +290,7 @@ ko.components.register('homePageTool', {
             </div>
         </div>
 
-        <div data-bind="foreach: displayModules(), sortable" id="sortableContainer">
+        <div data-bind="foreach: displayModules()" id="sortableContainer">
             <!-- ko component: {name: $data, params: { data: $parent } } --><!-- /ko -->
         </div>`, synchronous: true
 });
